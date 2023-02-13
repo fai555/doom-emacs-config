3db0df673509e609ad55c7d030786718db567bed
fshourove <ferdous.shourove@zalando.de>
2023-02-13 17:48:29 +0100

    Working Fast Enough

--- config.el
+++ config.el
@@ -31,7 +31,8 @@
 ;; Disable exit confirmation
 (setq confirm-kill-emacs nil)
 
-(setq org-superstar-headline-bullets-list '("⦿" "▶" "⦿" "▶" "⦿" "▶"))
+(setq org-superstar-headline-bullets-list '("❱" "❱" "❱" "❱" "❱" "❱"))
+;; (setq org-superstar-headline-bullets-list '("⦿" "▶" "⦿" "▶" "⦿" "▶"))
 
 (use-package org-bullets
   :hook (( org-mode ) . org-bullets-mode))
@@ -46,7 +47,7 @@
 
 ;; these are the defaults (before I changed them)
 (setq company-idle-delay 0.2
-      company-minimum-prefix-length 3);;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+      company-minimum-prefix-length 3)
 
 (defun my/org-mode-hook ()
   "Stop the org-level headers from increasing in height relative to the other text."
@@ -430,7 +431,7 @@
 
 (add-hook 'org-mode-hook 'org-appear-mode)
 
-(add-to-list 'default-frame-alist '(alpha . 95))
+;; (add-to-list 'default-frame-alist '(alpha . 95))
 
 
 ;; https://hieuphay.com/doom-emacs-config/
@@ -476,6 +477,7 @@
    org-modern-tag nil
    org-ellipsis "↵"
    org-modern-timestamp t
+   org-modern-star nil
    org-modern-statistics nil
    org-modern-progress nil
    org-modern-priority nil
@@ -485,71 +487,65 @@
                      (45 . "–")
                      (42 . "∘")))
 
-
-  (custom-set-faces!
-    `((org-modern-tag)
-      :background ,(doom-blend (doom-color 'blue) (doom-color 'bg) 0.1)
-      :foreground ,(doom-color 'grey))
-    `((org-modern-radio-target org-modern-internal-target)
-      :inherit 'default :foreground ,(doom-color 'blue)))
   )
 
 
 
-(use-package! svg-tag-mode
-  :config
-  (defconst date-re "[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}")
-  (defconst time-re "[0-9]\\{2\\}:[0-9]\\{2\\}")
-  (defconst day-re "[A-Za-z]\\{3\\}")
-  (defconst day-time-re (format "\\(%s\\)? ?\\(%s\\)?" day-re time-re))
+;; (use-package! svg-tag-mode
+;;   :config
+;;   (defconst date-re "[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}")
+;;   (defconst time-re "[0-9]\\{2\\}:[0-9]\\{2\\}")
+;;   (defconst day-re "[A-Za-z]\\{3\\}")
+;;   (defconst day-time-re (format "\\(%s\\)? ?\\(%s\\)?" day-re time-re))
 
-  (defun svg-progress-percent (value)
-    (svg-image (svg-lib-concat
-                (svg-lib-progress-bar
-                 (/ (string-to-number value) 100.0) nil
-                 :height 0.8 :background (doom-color 'bg)
-                 :margin 0 :stroke 2 :radius 3 :padding 2 :width 11)
-                (svg-lib-tag (concat value "%") nil
-                             :height 0.8 :background (doom-color 'bg)
-                             :stroke 0 :margin 0)) :ascent 'center))
+;;   (defun svg-progress-percent (value)
+;;     (svg-image (svg-lib-concat
+;;                 (svg-lib-progress-bar
+;;                  (/ (string-to-number value) 100.0) nil
+;;                  :height 0.8 :background (doom-color 'bg)
+;;                  :margin 0 :stroke 2 :radius 3 :padding 2 :width 11)
+;;                 (svg-lib-tag (concat value "%") nil
+;;                              :height 0.8 :background (doom-color 'bg)
+;;                              :stroke 0 :margin 0)) :ascent 'center))
 
-  (defun svg-progress-count (value)
-    (let* ((seq (mapcar #'string-to-number (split-string value "/")))
-           (count (float (car seq)))
-           (total (float (cadr seq))))
-      (svg-image (svg-lib-concat
-                  (svg-lib-progress-bar (/ count total) nil
-                                        :background (doom-color 'bg) :height 0.8
-                                        :margin 0 :stroke 2 :radius 3 :padding 2 :width 11)
-                  (svg-lib-tag value nil
-                               :background (doom-color 'bg)
-                               :stroke 0 :margin 0 :height 0.8)) :ascent 'center)))
+;;   (defun svg-progress-count (value)
+;;     (let* ((seq (mapcar #'string-to-number (split-string value "/")))
+;;            (count (float (car seq)))
+;;            (total (float (cadr seq))))
+;;       (svg-image (svg-lib-concat
+;;                   (svg-lib-progress-bar (/ count total) nil
+;;                                         :background (doom-color 'bg) :height 0.8
+;;                                         :margin 0 :stroke 2 :radius 3 :padding 2 :width 11)
+;;                   (svg-lib-tag value nil
+;;                                :background (doom-color 'bg)
+;;                                :stroke 0 :margin 0 :height 0.8)) :ascent 'center)))
 
-  (set-face-attribute 'svg-tag-default-face nil :family "Alegreya Sans")
-  (setq svg-tag-tags
-        `(;; Progress e.g. [63%] or [10/15]
-          ("\\(\\[[0-9]\\{1,3\\}%\\]\\)" . ((lambda (tag)
-                                              (svg-progress-percent (substring tag 1 -2)))))
-          ("\\(\\[[0-9]+/[0-9]+\\]\\)" . ((lambda (tag)
-                                            (svg-progress-count (substring tag 1 -1)))))
-          ;; Task priority e.g. [#A], [#B], or [#C]
-          ("\\[#A\\]" . ((lambda (tag) (svg-tag-make tag :face 'error :inverse t :height .85
-                                                     :beg 2 :end -1 :margin 0 :radius 10))))
-          ("\\[#B\\]" . ((lambda (tag) (svg-tag-make tag :face 'warning :inverse t :height .85
-                                                     :beg 2 :end -1 :margin 0 :radius 10))))
-          ("\\[#C\\]" . ((lambda (tag) (svg-tag-make tag :face 'org-todo :inverse t :height .85
-                                                     :beg 2 :end -1 :margin 0 :radius 10))))
-          ;; Keywords
-          ("TODO" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face 'org-todo))))
-          ("HOLD" . ((lambda (tag) (svg-tag-make tag :height .85 :face 'org-todo))))
-          ("DONE\\|STOP" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face 'org-done))))
-          ("NEXT\\|WAIT" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face '+org-todo-active))))
-          ("REPEAT\\|EVENT\\|PROJ\\|IDEA" .
-           ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face '+org-todo-project))))
-          ("REVIEW" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face '+org-todo-onhold))))))
+;;   (set-face-attribute 'svg-tag-default-face nil :family "Alegreya Sans")
+;;   (setq svg-tag-tags
+;;         `(;; Progress e.g. [63%] or [10/15]
+;;           ("\\(\\[[0-9]\\{1,3\\}%\\]\\)" . ((lambda (tag)
+;;                                               (svg-progress-percent (substring tag 1 -2)))))
+;;           ("\\(\\[[0-9]+/[0-9]+\\]\\)" . ((lambda (tag)
+;;                                             (svg-progress-count (substring tag 1 -1)))))
+;;           ;; Task priority e.g. [#A], [#B], or [#C]
+;;           ("\\[#A\\]" . ((lambda (tag) (svg-tag-make tag :face 'error :inverse t :height .85
+;;                                                      :beg 2 :end -1 :margin 0 :radius 10))))
+;;           ("\\[#B\\]" . ((lambda (tag) (svg-tag-make tag :face 'warning :inverse t :height .85
+;;                                                      :beg 2 :end -1 :margin 0 :radius 10))))
+;;           ("\\[#C\\]" . ((lambda (tag) (svg-tag-make tag :face 'org-todo :inverse t :height .85
+;;                                                      :beg 2 :end -1 :margin 0 :radius 10))))
+;;           ;; Keywords
+;;           ("TODO" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face 'org-todo))))
+;;           ("HOLD" . ((lambda (tag) (svg-tag-make tag :height .85 :face 'org-todo))))
+;;           ("DONE\\|STOP" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face 'org-done))))
+;;           ("NEXT\\|WAIT" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face '+org-todo-active))))
+;;           ("REPEAT\\|EVENT\\|PROJ\\|IDEA" .
+;;            ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face '+org-todo-project))))
+;;           ("REVIEW" . ((lambda (tag) (svg-tag-make tag :inverse t :height .85 :face '+org-todo-onhold))))))
 
-  :hook (org-mode . svg-tag-mode)
-  )
+;;   :hook (org-mode . svg-tag-mode)
+;;   )
+
 
 
 (use-package! org-appear
@@ -563,7 +559,7 @@
 (use-package git-gutter
   :hook (prog-mode . git-gutter-mode)
   :config
-  (setq git-gutter:update-interval 0.02))
+  (setq git-gutter:update-interval 0.10))
 
 (use-package git-gutter-fringe
   :config
@@ -575,3 +571,6 @@
 
 (use-package! verb
   :config (define-key org-mode-map (kbd "C-c C-r") verb-command-map))
+;;
+;; https://github.com/doomemacs/doomemacs/issues/2217
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(beacon with-editor git-gutter-fringe git-gutter org-modern company-shell magit-delta visual-fill-column evil-escape org-appear expand-region lsp-python-ms evil-vimish-fold evil-goggles lsp-ui verb tree-sitter-langs org-roam-ui svg-tag-mode)))
+   '(beacon with-editor git-gutter-fringe git-gutter org-modern company-shell magit-delta visual-fill-column evil-escape org-appear expand-region lsp-python-ms evil-vimish-fold evil-goggles lsp-ui verb tree-sitter-langs org-roam-ui)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
